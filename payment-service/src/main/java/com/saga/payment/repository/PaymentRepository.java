70cb97eb8a9aff1f0d65d5fca260fb1aa7e3dc75
mehmet-suer <mehmet.suer@linovi.com>
2026-02-28 00:01:22 +0300

    (fix) PaymentRepository updated

--- payment-service/src/main/java/com/saga/payment/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/saga/payment/repository/PaymentRepository.java
@@ -1,11 +1,18 @@
 package com.saga.payment.repository;
 
 import com.saga.payment.model.entity.Payment;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Optional<Payment> findByOrderId(UUID uuid);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Payment p where p.orderId = :orderId")
+    Optional<Payment> findByOrderIdForUpdate(@Param("orderId") UUID orderId);
 }
